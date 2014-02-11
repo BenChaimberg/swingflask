935c212d03318e93833eca9bc46d9733d71df6cb
Youppi3 <youppi3@gmail.com>
2014-02-11 14:00:17 -0500

    category pictures by row

--- flaskexample.py
+++ flaskexample.py
@@ -27,7 +27,7 @@ def product(productid):
 
 @app.route('/category/<categoryid>')
 def category(categoryid):
-    return render_template('category.html', category={'id':categoryid,'title':categorytitle.title[categoryid],'img':categoryimg.img[categoryid],'products':categoryproducts.products[categoryid]})
+    return render_template('category.html', category={'id':categoryid,'title':categorytitle.title[categoryid],'img':categoryimg.img[categoryid],'products':categoryproducts.products[categoryid],'counter':1})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
