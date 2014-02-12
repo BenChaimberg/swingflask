ae035fc19a7e10a62eac9a962bc6ab63f41e84ff
Youppi3 <youppi3@gmail.com>
2014-02-12 09:33:10 -0500

    product order

--- categoryproducts.py
+++ categoryproducts.py
@@ -1,8 +1,8 @@
 nontoxicproducts = {
-	'2101':'Terra Nova<br>Cream Polish',
 	'2104':'Terra Nova<br>Furniture Cleaner',
 	'2103':'Terra Nova<br>Liquid Pumice Stone',
 	'2102':'Terra Nova<br>Metal Restorer',
+	'2101':'Terra Nova<br>Cream Polish',
 	'2100':'Terra Nova<br>NaturOil',
 	'1823':'Circa 1850<br>Bare Concrete',
 	'1822':'Circa 1850<br>Bare Metal',
