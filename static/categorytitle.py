63123b58ca7ab5f4880e4919ff6e15dff02bc566
Youppi3 <youppi3@gmail.com>
2014-03-31 22:31:59 -0400

    image location change etc. #29

--- static/categorytitle.py
+++ static/categorytitle.py
@@ -1,4 +1,10 @@
 title = {
 'nontoxic':'Non-Toxic Products',
-'oils':'Oils, Varnishes &amp; Polyurethanes'
+'oils':'Oils, Varnishes &amp; Polyurethanes',
+'removers':'Paint &amp; Varnish Removers',
+'primer':'Paint &amp; Wallpaper Primers',
+'special':'Specialty Paints &amp; Coatings',
+'waxes':'Waxes, Polishes, &amp; Cleaners',
+'stain':'Wood Stains &amp; Varnish Stains',
+'accessories':'Accessories'
 }
