7696bf5e6be42bc59d8894f082aef28034c2c5c2
Youppi3 <youppi3@gmail.com>
2014-04-23 20:06:37 -0400

    add to brochure list #13

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
-import json
-x = {
-'1201':'Swing Paints Circa 1850 Chair-Loc Wood Sweller',
-
-'1300':'Swing Paints Circa 1850 Maple Scraper',
-'1301':'Swing Paints Circa 1850 Stripping Gloves',
-'1302':'Swing Paints Circa 1850 Stripping Tool',
-'1303':'Swing Paints Circa 1850 Finishing Pads',
-'1310':'Swing Paints Circa 1850 Cheese Cloth',
-'1315':'Swing Paints Circa 1850 Staining Essentials',
-'1316':'Swing Paints Circa 1850 Staining &amp, Mixing Essentials',
-
-'1418':'Swing Paints Circa 1850 Aqua Varnish'
-}
-with open('brochurelist', 'a') as file:
-	json.dump(x,file)
+import json,time
 with open('brochurelist', 'r') as file:
 	y = json.load(file)
-	print y['1201']
+	print time.strptime(y['0']['time'],"%a %b %d %H:%M:%S %Y")[0]
