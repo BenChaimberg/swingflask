6158eafaa04306fbc78836097cc8b6be551e81c2
Youppi3 <youppi3@gmail.com>
2014-05-10 19:57:04 -0400

    aqua colour swatches #39

--- flaskexample.py
+++ flaskexample.py
@@ -168,6 +168,13 @@ def colour(): #if URL is at about
 	else: #if URL does not end with ?french
 		return render_template('colour.html') #return english about page
 
+@app.route('/aquacolour')
+def aquacolour(): #if URL is at about
+	if request.query_string == 'french': #if URL ends with ?french
+		return render_template('frenchaquacolour.html') #render french about page
+	else: #if URL does not end with ?french
+		return render_template('aquacolour.html') #return english about page
+
 @app.route('/refer', methods=('GET', 'POST'))
 def refer():
 	form = ReferForm()
