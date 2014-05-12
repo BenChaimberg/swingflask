f635e5adce1f611ae656fac28cef64b9537bdf09
Paskunyak <paskunyak@aol.com>
2014-05-12 15:06:08 -0400

    Updated frenchproducttitle.py
    Inserted all items.  Inserted UNICODE for french characters.

--- static/frenchproducttitle.py
+++ static/frenchproducttitle.py
@@ -1,3 +1,100 @@
 title = {
-'1800':'Peintures Swing Circa 1850 D&#xe9;capant &#xe0; Meubles'
+'1201':'Peintures Swing Circa 1850 Chair-Loc Dilateur de bois',
+
+'1300':'Peintures Swing Circa 1850 Grattoir en &#xe9;rable',
+'1301':'Peintures Swing Circa 1850 Gants de dD&#xe9;capage',
+'1302':'Peintures Swing Circa 1850 Outil de dD&#xe9;capage',
+'1303':'Peintures Swing Circa 1850 Tampons de finition',
+'1310':'Peintures Swing Circa 1850 Coton Fromage',
+'1315':'Peintures Swing Circa 1850 Essentiels pour teindre',
+'1316':'Peintures Swing Circa 1850 Essentiels pour Teindre et MD&#xe9;lange',
+
+'1418':'Peintures Swing Circa 1850 Vernis Aqua',
+
+'1518':'Peintures Swing Circa 1850 Colle rD&#xe9;sine de plastique',
+'1527':'Peintures Swing Circa 1850 Nu-Lustre-27 au pinceau',
+'1555':'Peintures Swing Circa 1850 Nu-Lustre-55',
+'1557':'Peintures Swing Circa 1850 Nu-Lustre-57 UV-Resistant',
+'1566':'Peintures Swing Circa 1850 Nu-Lustre-66 Thermostable',
+
+'1600':'Peintures Swing Antiquax Cire',
+'1604':'Peintures Swing Antiquax Argent et cuivre',
+
+'1700':'Peintures Swing Gaudreault Antiques DD&#xe9;capant en liquide',
+'1702':'Peintures Swing Gaudreault Antiques Huile d\'abrasin',
+'1706':'Peintures Swing Gaudreault Antiques DD&#xe9;capant en p&#xe2;te',
+
+'1800':'Peintures Swing Circa 1850 DD&#xe9;capant &#xe0; meubles',
+'1801':'Peintures Swing Circa 1850 Repolisseur &#xe0; meubles',
+'1802':'Peintures Swing Circa 1850 Huile d’abrasin',
+'1803':'Peintures Swing Circa 1850 Huile de citron',
+'1804':'Peintures Swing Circa 1850 Nettoyeur &#xe0; meubles',
+'1805':'Peintures Swing Circa 1850 Vernis en p&#xe2;te antique',
+'1806':'Peintures Swing Circa 1850 DD&#xe9;capant en p&#xe2;te,
+'1807':'Peintures Swing Circa 1850 Huile Tung\’n Teak',
+'1809':'Peintures Swing Circa 1850 Woodturner\'s Jelly',
+'1815':'Peintures Swing Circa 1850 Vernis exterieur qualitD&#xe9; marine',
+'1817':'Peintures Swing Circa 1850 PolyurD&#xe9;thane sD&#xe9;chage rapide',
+'1818':'Peintures Swing Circa 1850 PolyurD&#xe9;thane sD&#xe9;chage rapide',
+'1819':'Peintures Swing Circa 1850 PolyurD&#xe9;thane sD&#xe9;chage rapide',
+'1820':'Peintures Swing Circa 1850 DD&#xe9;capant doux',
+'1821':'Peintures Swing Circa 1850 Nettoyeur pour plancher',
+'1822':'Peintures Swing Circa 1850 MD&#xe9;tal &#xe0; nu',
+'1823':'Peintures Swing Circa 1850 BD&#xe9;ton &#xe0; nu',
+'1824':'Peintures Swing Circa 1850 Bio DD&#xe9;Colleur',
+
+'1897':'Peintures Swing DD&#xe9;capants Professionels',
+
+'1900':'Peintures Swing Circa 1850 DD&#xe9;capage pour les nuls',
+
+'2100':'Peintures Swing Terra Nova NaturOil',
+'2101':'Peintures Swing Terra Nova Polis Crème',
+'2103':'Peintures Swing Terra Nova Restaurateur &#xe0; MD&#xe9;tal',
+'2103':'Peintures Swing Terra Nova Pierre Ponce Liquide',
+'2104':'Peintures Swing Terra Nova Nettoyeur &#xe0; meubles',
+
+'2140':'Peintures Swing Circa 1850 Huile de noix pure',
+'2142':'Peintures Swing Circa 1850 Huile d’abrasin crue',
+'2143':'Peintures Swing Circa 1850 Huile d’abrasin polymerizD&#xe9;',
+'2146':'Peintures Swing Circa 1850 Huile de lin crue',
+'2147':'Peintures Swing Circa 1850 Huile de lin bouillie',
+
+'2705':'Peintures Swing Circa 1850 Vernis en p&#xe2;te – QualitD&#xe9; Marine',
+'2706':'Peintures Swing Circa 1850 DD&#xe9;capant en p&#xe2;te – QualitD&#xe9; Marine',
+'2707':'Peintures Swing Circa 1850 Huile Tung’n Teak - QualitD&#xe9; Marine',
+'2711':'Peintures Swing Circa 1850 Diluant Epoxy - QualitD&#xe9; Marine',
+'2752':'Peintures Swing Circa 1850 Email Epoxy - QualitD&#xe9; Marine',
+'2760':'Peintures Swing Circa 1850 Huile Antique – QualitD&#xe9; Marine',
+
+'3100':'Peintures Swing Circa 1850 Prime-It',
+'3107':'Peintures Swing Circa 1850 Prep-Coat pour tapisserie',		
+'3133':'Peintures Swing Circa 1850 Prime-It Plus',
+'3150':'Peintures Swing Circa 1850 Hang-Rite pour tapisserie',	
+
+'3200':'Peintures Swing Circa 1850 Super DeGooper',
+'3207':'Peintures Swing Klenk\'s Nettoyeur pour colle contact,
+'3208':'Peintures Swing Circa 1850 Anti-Grafitti',
+'3211':'Peintures Swing Circa 1850 Sableuse liquide',
+'3220':'Peintures Swing Circa 1850 DD&#xe9;colorant pour bois',
+'3230':'Peintures Swing Circa 1850 Super DD&#xe9;Colleur',
+
+'400':'Peintures Swing Circa 1850 Aqua Teinture &#xe0; bois',
+
+'705':'Peintures Swing Circa 1850 Apprêt &#xe0; bois',
+'709':'Peintures Swing Circa 1850 Teinture &#xe0; bois',
+
+'760':'Peintures Swing Circa 1850 Huile antique danoise',
+
+'770':'Peintures Swing Circa 1850 Stain\'n Varnish',
+
+'8100':'Peintures Swing Klenk\'s Epoxy Formule Suisse',
+'8172':'Peintures Swing Klenk\'s Suppressur d'yeux de poisson',	
+'8200':'Peintures Swing Klenk\'s ACR Formule Suisse',
+
+'8999':'Peintures Swing Klenk\'s Diluant &#xe0; laque',
+'9000':'Peintures Swing Klenk\'s Diluant &#xe0; peinture inodore',
+'9001':'Peintures Swing Klenk\'s AcD&#xe9;tone',
+'9006':'Peintures Swing Klenk\'s Hydrate de mD&#xe9;thyle',	
+'9008':'Peintures Swing Klenk\'s Toluène',
+'9019':'Peintures Swing Klenk\'s Essences minD&#xe9;rale'
 }
