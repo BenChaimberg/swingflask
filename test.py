6d660f73532227dfc0f10a16f5b7cf2cf56a688a
Youppi3 <youppi3@gmail.com>
2014-07-30 04:00:36 -0400

    python parsing of HTML for french producttext #38

--- test.py
+++ test.py
@@ -1,6 +1,52 @@
-import json
-with open('brochurelist', 'r') as file:
-	y = json.load(file)
-	for x in range(0,2679):
-		for z in y[str(x)]:
-			print y[str(x)][z]
+from HTMLParser import HTMLParser
+from htmlentitydefs import name2codepoint
+import urllib2
+from urllib2 import Request, urlopen, URLError
+from static import frenchproducttitle
+
+for x in frenchproducttitle.title:
+    req = Request('http://www.swingpaints.com/french/'  + str(x) + '.htm')
+    try:
+        response = urlopen(req)
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server. Reason: ', e.reason
+        elif hasattr(e, 'code'):
+            print '\''+x+'\':\'\'\'404\'\'\','
+    else:
+        html = response.read()
+        html = html.decode('latin-1')
+    
+        class MyHTMLParser(HTMLParser):
+        	def __init__(self):
+        		HTMLParser.__init__(self)
+        		self.recording = False
+        		self.datalist = []
+        	def handle_starttag(self, tag, attrs):
+        		if tag == 'p':
+        			self.recording = True
+        	def handle_endtag(self, tag):
+        		if tag == 'p':
+        			self.recording = False
+        	def handle_data(self, data):
+        		if self.recording == True:
+        			self.datalist.append(data)
+        	def handle_entityref(self, name):
+        		c = unichr(name2codepoint[name])
+        		if self.recording == True:
+        			self.datalist.append('&'+name+';')
+        	
+    		def handle_charref(self, name):
+        		if name.startswith('x'):
+        		    c = int(name[1:], 16)
+        		else:
+        		    c = int(name)
+        		if self.recording == True:
+        			self.datalist.append(c)
+        
+        parser = MyHTMLParser()
+        parser.feed(html)
+        wholedata = ''
+        for datum in parser.datalist:
+        	wholedata += datum
+        print '\''+x+'\':\'\'\''+wholedata+'\'\'\','
