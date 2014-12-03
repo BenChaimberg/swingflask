48e413b6f257916ac679c2cde25f1e26d4ad4a2f
Youppi3 <youppi3@gmail.com>
2014-12-03 00:03:12 -0500

    product mobile view initial

--- __init__.py
+++ __init__.py
@@ -311,4 +311,4 @@ def category(categoryid):
 		else: abort(404)
 
 if __name__ == '__main__': #only run if executed directly from interpreter
-    app.run(debug=True,host= '0.0.0.0') #run server with application (debug on, must be turned off for deployment)
+    app.run(debug=False,host='0.0.0.0') #run server with application (debug on, must be turned off for deployment)
