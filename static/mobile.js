2fef5831ddaaaf8d78d94877f0ac27aa9d6fa952
Youppi3 <youppi3@gmail.com>
2015-06-27 18:33:26 -0400

    indent/spacing minor fix

--- static/mobile.js
+++ static/mobile.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 		$("#category-list").prepend("<li>"+$("#category-heading").html()+"</li>");
 		$("#brand-list").prepend("<li>"+$("#brand-heading").html()+"</li>");
 		$("#mobile-header-left").click(function(){
- 			var currentPosition = $(document).scrollTop();
+			var currentPosition = $(document).scrollTop();
 			$("#sidebar").animate({ marginLeft: "0%"} , 500);
- 			$("#sidebar").css({"-webkit-overflow-scrolling": "touch"});
+			$("#sidebar").css({"-webkit-overflow-scrolling": "touch"});
 			$("body").css({"overflow":"hidden"});
 			$("body").css({"position":"fixed"});
 			$("body").css({"width":$(window).width()});
@@ -17,7 +17,7 @@ $(document).ready(function(){
 		});
 		$("#sidebar-before").click(function(){
 			$("#sidebar").animate({ marginLeft: "-100%"} , 500);
- 			$("#sidebar").css({"-webkit-overflow-scrolling": "auto"});
+			$("#sidebar").css({"-webkit-overflow-scrolling": "auto"});
 			$("body").css({"overflow":"default"});
 			$("body").css({"position":"default"});
 			$('html, body').animate({scrollTop:currentPosition});
