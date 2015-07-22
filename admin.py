90b11cddbf1e6c7090490fadc341cef10b2c1ee6
Youppi3 <youppi3@gmail.com>
2015-07-22 00:34:27 -0400

    remove static admin edit

--- admin.py
+++ admin.py
@@ -124,31 +124,11 @@ class InfoListModelView(CustomModelView):
 
 class CustomFileAdmin(FileAdmin):
     list_template = 'admin/right_links_file.html'
-
-    def git_commit(self, name):
-        bashCommand = 'git commit -m "admin_edit_' + str(name) + '"'
-        import subprocess
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output = process.communicate()[0]
-        print output
-
-    def git_add(self, name):
-        bashCommand = 'git add ' + str(name)
-        import subprocess
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output = process.communicate()[0]
-        print output
-
-    def git_rm(self, name):
-        bashCommand = 'git rm ' + str(name)
-        import subprocess
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output = process.communicate()[0]
-        print output
-
-    def on_file_upload(self, full_path, dir_base, filename):
-        self.git_add(filename)
-        self.git_commit(filename)
+    can_upload = False
+    can_delete = False
+    can_delete_dirs = False
+    can_mkdir = False
+    can_rename = False
 
     def is_accessible(self):
         return current_user.is_authenticated()
