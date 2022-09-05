40dc902e36e213e4db3e7d54953713e8846d7368
Ben Chaimberg <youppi3@gmail.com>
2022-09-05 19:37:16 -0400

    allow gallery feature description to have unlimited length

--- models.py
+++ models.py
@@ -295,7 +295,7 @@ class Gallery(db.Model):
     id = db.Column(db.Integer(), primary_key=True, nullable=False)
     title = db.Column(db.String(50), nullable=True)
     path = db.Column(db.String(50), nullable=True)
-    description = db.Column(db.String(250), nullable=True)
+    description = db.Column(db.Text(), nullable=True)
 
     def __init__(
         self,
